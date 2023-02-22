5dea5d590ce7fdd5b8f75f3796964152a2c91a0a
a.hryhoriev <andrii.hryhoriev@levi9.com>
2023-02-22 12:58:47 +0200

    add one more test

--- src/test/java/com/levi9/smdb/integration/core/BaseIntegrationTest.java
+++ src/test/java/com/levi9/smdb/integration/core/BaseIntegrationTest.java
@@ -3,13 +3,11 @@ package com.levi9.smdb.integration.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
-@ActiveProfiles("test")
 public class BaseIntegrationTest {
 
     private static final String DOCKER_IMAGE_NAME = "postgres:14.7-alpine";
--- src/test/java/com/levi9/smdb/integration/test/DepartmentsRepositoryTest.java
+++ src/test/java/com/levi9/smdb/integration/test/DepartmentsRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.levi9.smdb.integration.test;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.levi9.smdb.dto.DepartmentDTO;
 import com.levi9.smdb.entity.Department;
 import com.levi9.smdb.integration.core.BaseIntegrationTest;
 import com.levi9.smdb.repository.DepartmentRepository;
@@ -22,6 +25,13 @@ class DepartmentsRepositoryTest extends BaseIntegrationTest {
 
     @Test
     void NotEmptyRepository() {
+        List<DepartmentDTO> allDepartments = departmentRepository.getAllDepartments();
+        assertTrue(allDepartments.size() > 0);
+        System.out.println(allDepartments.size());
+    }
+
+    @Test
+    void shouldBeCertainDepartmentName() {
         Department dept = departmentRepository.getDepartmentById(0L);
         assertEquals("TestDepartmentName", dept.getDepName());
 
